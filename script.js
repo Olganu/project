e95cc2d16023e7bda19c662af683d6f04a2422bb
Olga <example@gmail.com>
2021-02-01 16:14:59 +0200

    index.html mod

---
+++ script.js
@@ -0,0 +1,20 @@
+const numberOrfFils = prompt('Сколько фильмщв вы уже посмотрели?' ,'');
+
+const personalMovieDB = {
+    count: numberOrfFils,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов', ''),
+    b = prompt('Насколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов', ''),
+    d = prompt('Насколько оцените его?', '');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
+
